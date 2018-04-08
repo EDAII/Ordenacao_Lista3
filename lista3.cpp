3dd74ec9388dad272f0f9caa065ffc3d45e25557
joaovitorml <joao.lemos4000@gmail.com>
2018-04-08 10:02:18 -0300

    Added heap sort.

--- lista3.cpp
+++ lista3.cpp
@@ -1,5 +1,7 @@
-#include <iostream>
+#include <bits/stdc++.h>
 #include <algorithm>
+#include <sys/time.h>
+#include <sys/resource.h>
 
 using namespace std;
 
@@ -42,8 +44,51 @@ void quick_sort (int *array, int inicio, int fim){
 
 }
 
+void heap_sort(int *array, int n) {
+   int i = n / 2, pai, filho, t;
+   while(true) {
+      if (i > 0) {
+          i--;
+          t = array[i];
+      } else {
+          n--;
+          if (n == 0) return;
+          t = array[n];
+          array[n] = array[0];
+      }
+      pai = i;
+      filho = i * 2 + 1;
+      while (filho < n) {
+          if ((filho + 1 < n)  &&  (array[filho + 1] > array[filho]))
+              filho++;
+          if (array[filho] > t) {
+             array[pai] = array[filho];
+             pai = filho;
+             filho = pai * 2 + 1;
+          } else {
+             break;
+          }
+      }
+      array[pai] = t;
+   }
+}
+
+double calcula_tempo(const struct rusage *b, const struct rusage *a) {
+    if (b == NULL || a == NULL)
+        return 0;
+    else
+        return ((((a->ru_utime.tv_sec * 1000000 + a->ru_utime.tv_usec) -
+                 (b->ru_utime.tv_sec * 1000000 + b->ru_utime.tv_usec)) +
+                ((a->ru_stime.tv_sec * 1000000 + a->ru_stime.tv_usec) -
+                 (b->ru_stime.tv_sec * 1000000 + b->ru_stime.tv_usec)))
+                / 1000000.0);
+}
+
+
 int main(){
 
+  struct rusage tempo_inicial, tempo_final;
+  double tempo,tempo2;
 	int aux; //"Aux" é a variável que representa o número de tubos presentes no vetor
   int i,j; //Variáveis auxiliares para realização dos loops
 
@@ -58,7 +103,16 @@ int main(){
     vetor[i] = rand()%100000;
   }
 
-  quick_sort(vetor,0,aux);
+  getrusage(RUSAGE_SELF, &tempo_inicial);
+    quick_sort(vetor,0,aux);
+  getrusage(RUSAGE_SELF, &tempo_final);
+  tempo = calcula_tempo(&tempo_inicial, &tempo_final);
+
+  getrusage(RUSAGE_SELF, &tempo_inicial);
+    heap_sort(vetor,aux);
+  getrusage(RUSAGE_SELF, &tempo_final);
+  tempo2 = calcula_tempo(&tempo_inicial, &tempo_final);
+
 
   if(aux <= 20){
     cout << "Vetor ordenado = " << endl;
@@ -67,5 +121,9 @@ int main(){
     }
     cout << endl;
   }
+
+  printf("Tempo Quick Sort: %lf\n",tempo);
+  printf("Tempo Heap Sort: %lf\n",tempo2);
+
 	return 0;
 }
